8e53a74849bd2efc4c230b475d95a0f987748855
lmmx <louismmx@gmail.com>
2016-11-15 11:23:36 +0000

    Clipboard parcellite package

--- programmatic/3_apt/apt-get.sh
+++ programmatic/3_apt/apt-get.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 #shell:
 #-terminal management, full unicode in system font
 sudo apt-get install tmux ttf-ancient-fonts
-
+sudo apt-get install parcellite
 sudo apt-get install gedit
 
 sudo apt-get install build-essential python-numpy cmake python-dev sqlite3 libsqlite3-dev libboost-dev python-dev python3-dev
